064aafba2a00a4d423915fe3ea5b239b3f29ec16
msy7822-ux <msy780022@gmail.com>
2023-05-24 11:39:28 +0900

    modify suspense insert

--- src/app/actions/pai.ts
+++ src/app/actions/pai.ts
@@ -5,11 +5,13 @@ import { PaiType } from "@/types/paiType";
 import { v4 as uuidv4 } from "uuid";
 
 export const createVote = async (situationId: string, answer: PaiType) => {
-  const { error } = await supabase.from("votes").insert({
-    id: uuidv4(),
-    situationId: situationId,
-    answer: answer,
-  });
+  const { error } = await supabase.from("votes").insert([
+    {
+      id: uuidv4(),
+      situationId: situationId,
+      answer: answer,
+    },
+  ]);
 
   console.log(error);
 };
