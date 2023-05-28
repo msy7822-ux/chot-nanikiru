28b78251567cfa94fbdd1eb86df7ebb4f561761c
msy7822-ux <msy780022@gmail.com>
2023-05-28 10:58:57 +0900

    Revert "use revalidatePath🗿"
    This reverts commit 341c5696991c4eff7aaa1304145226ddce6e9afe.

--- src/app/actions/pai.ts
+++ src/app/actions/pai.ts
@@ -3,22 +3,15 @@
 import { supabase } from "@/libs/supabase";
 import { PaiType } from "@/types/paiType";
 import { v4 as uuidv4 } from "uuid";
-import { revalidatePath } from "next/cache";
 
 export const createVote = async (situationId: string, answer: PaiType) => {
   const uuid = uuidv4();
 
-  try {
-    const { error } = await supabase.from("votes").insert({
-      id: uuid,
-      situation_id: situationId,
-      answer: answer,
-    });
+  const { error } = await supabase.from("votes").insert({
+    id: uuid,
+    situation_id: situationId,
+    answer: answer,
+  });
 
-    console.log("error is ", error);
-  } catch (error) {
-    console.error("error is ", error);
-  } finally {
-    revalidatePath(`/kinma/${situationId}`);
-  }
+  console.log("error is ", error);
 };
